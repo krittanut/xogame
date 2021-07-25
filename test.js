1eaa370d184d1d0287473b36b7fa290c3ad4f2d7
krittanut <sangsri.krittanut@gmail.com>
2021-07-25 10:11:11 +0700

    change checkwin function

--- test.js
+++ test.js
@@ -1,146 +1,94 @@
-const cars = ["Saab", "Volvo", "BMW", "", ""];
-console.log(cars);
-console.log(availableMove(cars));
-
-// var sizexo = 3;
-
-
 // X == 1
 // O == -1
 // not pick == 0 
-// var test = [ -1, 1, 1,
-//              -1, -1, 1,
-//              1, -1, -1];
+var test = [ 1, 0, 0, 1,
+             1, 0, 0, 1,
+             1, 0, 0, 1,
+             1, 1, 1, 1];
 
-// checkwin(test,8);
+// checkwin(test,6,4);
+
+var test = [ 1, 0, 0, 1,
+             1, 0, 0, 1,
+             1, 0, 0, 1,
+             1, 1, 1, 1];
+checkwin(test,0,4);
 
 function checkwin(checkboard,num,sizexo) {
-    var vertical = 0;
-    var horizon = 0;  
-    var diago = 0 ;
-
-    // checkFirstValue  3 possible vertical horizon diagonal
-    if(num == 0) {
-        // check left top conner 
-        for(let i = 0 ; i < sizexo ; i ++) {
-            // vertical index 0 
-            console.log("vertical " + checkboard[i] );
-            vertical+=checkboard[i];
-            console.log(vertical);
-            // horizon index  0
-            console.log("horizon " + checkboard[i*sizexo] );
-            horizon+=checkboard[i*3];
-            console.log(horizon);
-            // 
-            console.log("diago " + checkboard[i*(sizexo+1)] );
-            diago+=checkboard[i*4];
-            console.log(diago);
-
-            // if(vertical  == 3 || horizon == 3 || diago == 3 ){
-            //     console.log("X WINNNN"); 
-            // }else if(vertical  == -3 || horizon == -3 || diago == -3 ) {
-            //     console.log("O WINNN");
-            // }else {
-            //     console.log("No one win in thsi move");
-            // }
-            whowin(vertical,horizon,diago);
-
-            }
+    var allposible =  allposiblewin(sizexo) ; 
+    // console.log(allposible);
+    for(let i in allposible) {
+        var count = 0;
+        console.log(allposible[i]);
+        for(let b = 0 ; b < sizexo ; b++) {
+            // console.log(allposible[i][b]);
+            count+=checkboard[allposible[i][b]];
         }
-    else if(num/sizexo == sizexo-1 ) {
-        //check left bottom conner
-        for(let i = 0; i<sizexo ; i ++) {            
-            // console.log(" Check num+i " + (num+i));
-            console.log("vertical " + checkboard[i*sizexo] );
-            vertical+=checkboard[i*sizexo];
-            console.log("Check number   :  " + (i*sizexo));
-            console.log(vertical);
-
-            console.log("horizon " + checkboard[num+i] );
-            horizon+=checkboard[num+i];
-            console.log("Check number   :  " + (num+i) );
-            console.log(horizon);
-            
-            
-            console.log("diago " + checkboard[(i+1)*(sizexo-1)] );
-            diago+=checkboard[(i+1)*(sizexo-1)];
-            console.log("Check number   :  " + (i+1)*(sizexo-1) );
-            console.log(diago);  
-
-            // if(vertical  == 3 || horizon == 3 || diago == 3 ){
-            //     console.log("X WINNNN"); 
-            // }else if(vertical  == -3 || horizon == -3 || diago == -3 ) {
-            //     console.log("O WINNN");
-            // }else{
-            //     console.log("Noone win in this move");
-            // }ical,horizon,diago);
-            whowin(vertical,horizon,diago);
-
-        
-        }
-    }else if(num == sizexo-1) {
-        //check right top conner
-        for(let i = 0; i<sizexo ; i ++) {            
-            console.log("vertical " + checkboard[((i+1)*sizexo)-1] );
-            vertical+=checkboard[((i+1)*sizexo)-1];
-            console.log("Check number   :  " + (((i+1)*sizexo)-1));
-            console.log(vertical);
-
-            console.log("horizon " + checkboard[i] );
-            horizon+=checkboard[i];
-            console.log("Check number   :  " + (i) );
-            console.log(horizon);
-            
-            console.log("diago " + checkboard[(i+1)*(sizexo-1)] );
-            diago+=checkboard[(i+1)*(sizexo-1)];
-            console.log("Check number   :  " + (i+1)*(sizexo-1) );
-            console.log(diago);  
-            // if(vertical  == 3 || horizon == 3 || diago == 3 ){
-            //     console.log("X WINNNN"); 
-            // }else if(vertical  == -3 || horizon == -3 || diago == -3 ) {
-            //     console.log("O WINNN");
-            // }else{
-            //     console.log("Noone win in this move");
-            // }
-            whowin(vertical,horizon,diago);
-
+        console.log(count);
+        whowin(count,sizexo);
+        // console.log(allposible[i]);
     }
-    }else if(num == (sizexo**2)-1) {
-        //check right bottom conner
-        for(let i = 0; i<sizexo ; i ++) {            
-            console.log("vertical " + checkboard[((i+1)*sizexo)-1] );
-            vertical+=checkboard[((i+1)*sizexo)-1];
-            console.log("Check number   :  " + (((i+1)*sizexo)-1));
-            console.log(vertical);
 
-            console.log("horizon " + checkboard[i+(sizexo*(sizexo-1))] );
-            horizon+=checkboard[i+(sizexo*(sizexo-1))];
-            console.log("Check number   :  " + (i+(sizexo*(sizexo-1))));
-            console.log(horizon);
-            
-            console.log("diago " + checkboard[i*(sizexo+1)] );
-            diago+=checkboard[i*(sizexo+1)];
-            console.log("Check number   :  " + (i*(sizexo+1)));
-            console.log(diago);
-
-            // if(vertical  == 3 || horizon == 3 || diago == 3 ){
-            //     console.log("X WINNNN"); 
-            // }else if(vertical  == -3 || horizon == -3 || diago == -3 ) {
-            //     console.log("O WINNN");
-            // }else{
-            //     console.log("Noone win in this move");
-            // }
-            whowin(vertical,horizon,diago);
-    }
-    
-    }
 }
 
-function whowin(vertical,horizon,diago) {
-    if(vertical  == 3 || horizon == 3 || diago == 3 ){
+// allposiblewin(5);
+function allposiblewin(sizexo) {
+    var allpossible = []
+    //vertical
+    for(let i = 0 ; i < sizexo ; i ++) {
+        // console.log(i);
+        var numupdate = i ; 
+        var all = [] 
+        for(let b = 0 ; b <sizexo ; b++) {
+            // console.log(numupdate );
+            all[b] = numupdate; 
+            numupdate+=sizexo;
+ 
+        }
+        allpossible.push(all);
+    }
+    // console.log(allvertical); 
+
+    // horizon 
+    for(let i = 0 ; i <sizexo ; i++) {
+        var all = [] 
+        for(let b = 0 ; b < sizexo ; b++) {
+            // console.log(b + (sizexo * i));
+            all.push(b + (sizexo * i));
+        }
+        allpossible.push(all);
+        // console.log(all);
+    }
+    // console.log(allpossible);
+    //diago 
+    for(let i = 0 ; i < 2 ; i++) {
+        var all = [] ;
+        if(i != 0 ) {
+            var diago1 = sizexo-1;
+        }
+        for (let b = 0 ; b < sizexo ;b ++) {
+            if ( i == 0 ) {
+                // console.log(b * (sizexo +1));
+                all.push(b * (sizexo +1));
+            }else {
+                // console.log((sizexo-1)*(b+1));
+                all.push((sizexo-1)*(b+1));
+            }
+
+        }
+        // console.log(all);
+        allpossible.push(all);
+    }
+    // console.log(allpossible.length);
+    return allpossible 
+}
+
+
+function whowin(count,sizexo) {
+    if(count  == sizexo){
         console.log("X WINNNN"); 
         // window.alert("X WINNNN");
-    }else if(vertical  == -3 || horizon == -3 || diago == -3 ) {
+    }else if(count  == -sizexo) {
         console.log("O WINNN");
         // window.alert("O WINNN");
     }else{
